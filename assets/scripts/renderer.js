78a122366026ab17a172fc6e0ba82870944b4513
GreenCommunications <alexander.m.rankin@gmail.com>
2018-12-31 23:37:53 -0500

    added banner

--- assets/scripts/renderer.js
+++ assets/scripts/renderer.js
@@ -1,3 +1,6 @@
 window.onload = () => 
 {
+    document.getElementById("notice-close").addEventListener("click", () => {
+        document.getElementById("banner").style.display = "none";
+    });
 }
