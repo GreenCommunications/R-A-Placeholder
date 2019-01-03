a3517bed5e3999335eac0fbe9976d912fbb99cc4
GreenCommunications <alexander.m.rankin@gmail.com>
2019-01-02 22:53:08 -0500

    added fonts and switch to desktop

--- assets/scripts/renderer.js
+++ assets/scripts/renderer.js
@@ -1,6 +1,11 @@
 window.onload = () => 
 {
-    document.getElementById("notice-close").addEventListener("click", () => {
-        document.getElementById("banner").style.display = "none";
+    document.getElementById("enableDesktopSite").addEventListener("click", () => 
+    {
+        viewport = document.querySelector("meta[name=viewport]");
+viewport.setAttribute('content', 'width=960px, initial-scale=1.0, maximum-scale=1.0, user-scalable=0');
+
+        document.getElementById("mobile").style.display = "none";
+        document.getElementById("desktop").style.display = "block";
     });
 }
