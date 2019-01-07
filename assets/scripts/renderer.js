20e1c37de4aef0c7d8b73d45cae2d9710a708570
GreenCommunications <alexander.m.rankin@gmail.com>
2019-01-06 23:01:04 -0500

    adjusted header styling

--- assets/scripts/renderer.js
+++ assets/scripts/renderer.js
@@ -1,3 +1,5 @@
+var formSubmitted = false;
+
 window.onload = () => 
 {
     document.getElementById("enableDesktopSite").addEventListener("click", () => 
@@ -7,5 +9,32 @@ viewport.setAttribute('content', 'width=960px, initial-scale=1.0, maximum-scale=
 
         document.getElementById("mobile").style.display = "none";
         document.getElementById("desktop").style.display = "block";
+    });
+
+    document.getElementById("notice-close").addEventListener("click", () => 
+    {
+        document.getElementById("banner").style.display = "none";
+    });
+
+    document.getElementById("submitButton").addEventListener("click", () =>
+    {
+        document.getElementById("submitButton").style.backgroundColor = "#00d7d2";
+        document.getElementById("submitButton").innerHTML = "&check;";
+
+        formSubmitted = true;
+    });
+    document.getElementById("submitButton").addEventListener("mouseover", () =>
+    {
+        if(!formSubmitted)
+        {
+            document.getElementById("submitButton").style.backgroundColor = "#031b4e";
+        }
+    });
+    document.getElementById("submitButton").addEventListener("mouseout", () =>
+    {
+        if(!formSubmitted)
+        {
+            document.getElementById("submitButton").style.backgroundColor = "#0069ff";
+        }
     });
 }
